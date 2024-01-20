5c964e5ddaf2ad8ccf0c9bca71686e0301b3ae91
commanderxa <art.ostrikov@gmail.com>
2024-01-21 01:31:12 +0600

    Data module code refactor

--- src/data/dataset.rs
+++ src/data/dataset.rs
@@ -1,7 +1,13 @@
+/// # Dataset Trait
+///
+/// Defines the behavior of `Dataset`.
+/// Methods:
+/// - len - returns the length of the dataset. E.g. number of rows from a table
+/// - sample - returns a sample given the index
 pub trait Dataset<T> {
+    /// Returns length of `Dataset`
     fn len(&self) -> usize;
-    fn is_empty(&self) -> bool {
-        self.len() == 0
-    }
+
+    /// Provides a Sample of <T> given the index
     fn sample(&self, index: usize) -> T;
 }
